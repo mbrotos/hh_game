f4af6e9008fd670a5c825bf6421fd33a9ff2da72
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-03-19 16:25:07 -0400

    added hobo

--- Player.py
+++ Player.py
@@ -24,21 +24,20 @@ class Player:
         self.L1 = L1
         self.S = S
 
+import random
+
 #Hobo inhereits player
-class Hobo:
+class Hobo(Player):
     """
     Main encapsulates a hogwartz game and helping functions.
     """
     #State variables
-    M = None
-    L0 = None
-    L1 = None
-    S = None
+    messagesList = [[0,""],[1,""]] #0 meaning no varible, 1 meaning track number needed.
 
     
     def __init__(self, M, L0, L1, S):
         """
-        Construct a new 'Main' object.
+        Construct for a non-playing hobo.
 
         :param M: number of tracks
         :param L0: Time it takes for a train to pass through a tunnel.
@@ -50,3 +49,17 @@ class Hobo:
         self.L0 = L0
         self.L1 = L1
         self.S = S
+
+
+    def airPlaneMsg(self):
+        """
+        Selects a random message from the Hobo.
+
+        :param self: the current hobo object
+        :return: a string containing a message from the hobo
+        """
+        msgChoice = random.choice(self.messagesList)
+        if (msgChoice[0]==0):
+            return msgChoice[1]
+        else:
+            return msgChoice[1].format(super.currentTrack)
