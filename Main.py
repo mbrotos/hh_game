4252d5efd794f492cbdf31e274fbb09c6359ab96
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-03-19 15:17:11 -0400

    pydoc

--- Main.py
+++ Main.py
@@ -1,12 +1,24 @@
 class Main:
+    """
+    Main encapsulates a hogwartz game and helping functions.
+    """
     #State variables
     M = None
     L0 = None
     L1 = None
     S = None
 
-    #Constructor
+    
     def __init__(self, M, L0, L1, S):
+        """
+        Construct a new 'Main' object.
+
+        :param M: number of tracks
+        :param L0: Time it takes for a train to pass through a tunnel.
+        :param L1: Time it takes for the next train to arrive.
+        :param S: Time that the player stays on each track before jumping to another.
+        :return: returns nothing
+        """
         self.M = M
         self.L0 = L0
         self.L1 = L1
