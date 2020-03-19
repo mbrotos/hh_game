28e19a995c2be0ec4a3dbfadd5d8746ed68cb873
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-03-19 15:08:08 -0400

    init

---
+++ Main.py
@@ -0,0 +1,14 @@
+class Main:
+    #State variables
+    M = None
+    L0 = None
+    L1 = None
+    S = None
+
+    #Constructor
+    __init__(self, M, L0, L1, S):
+        self.M = M
+        self.L0 = L0
+        self.L1 = L1
+        self.S = S
+    
