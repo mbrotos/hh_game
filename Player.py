c7ace4e3daf3bd6295f2927e150755affee458de
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-03-20 13:58:49 -0400

    Adam

--- Player.py
+++ Player.py
@@ -1,3 +1,5 @@
+import random
+from Main import Main
 class Player:
     """
     Player encapsulates a hogwartz game and helping functions.
@@ -29,29 +31,30 @@ class Player:
     def __str__(self):
         return "Player is on track: {} and has {} health. ".format(self.current_track, self.health)
 
-#Hobo inhereits player
-class Hobo:
+class Hobo(Player):
     """
     Main encapsulates a hogwartz game and helping functions.
     """
     #State variables
-    M = None
-    L0 = None
-    L1 = None
-    S = None
+    messagesList = ["",""] 
 
     
-    def __init__(self, M, L0, L1, S):
+    def __init__(self, health = None, current_track = None):
         """
-        Construct a new 'Main' object.
+        Construct for a hobo, whether player or non-player(default non-player).
 
-        :param M: number of tracks
-        :param L0: Time it takes for a train to pass through a tunnel.
-        :param L1: Time it takes for the next train to arrive.
-        :param S: Time that the player stays on each track before jumping to another.
+        :param health: The starting health of the player
+        :param current_track: The track that the player is currently on.
         :return: returns nothing
         """
-        self.M = M
-        self.L0 = L0
-        self.L1 = L1
-        self.S = S
+        super(health, current_track)
+
+
+
+    def airPlaneMsg(self):
+        """
+        Selects a random message from the Hobo.
+
+        :return: a string containing a message from the hobo
+        """
+        return (random.choice(self.messagesList)).format(random.randint(1,Main.M))
